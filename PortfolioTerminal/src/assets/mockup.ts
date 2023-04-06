7a83e9e4e373251fd86611300ce3b17311abe83b
Eryk Olsza <79854472+Kyt00n@users.noreply.github.com>
2023-04-06 18:12:44 +0200

    header and footer done

--- PortfolioTerminal/src/assets/mockup.ts
+++ PortfolioTerminal/src/assets/mockup.ts
@@ -19,3 +19,26 @@ enum Menu {
     //my contact details
     CONTACT_ME
 }
+/////////////////////////////
+const gui = new dat.GUI()
+const parameters = {
+    materialColor: '#ffeded'
+}
+
+gui
+    .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+})
+
+const canvas = document.querySelector('canvas.webgl')
+// Scene
+const scene = new THREE.Scene()
+
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('textures/matcaps/7.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
+const material = new THREE.MeshToonMaterial({color: parameters.materialColor, gradientMap: gradientTexture})
+const wordCloud = new THREE.Mesh()
+const fontLoader = new FontLoader()
--- PortfolioTerminal/src/components/terminalComponent/terminalComponent.tsx
+++ PortfolioTerminal/src/components/terminalComponent/terminalComponent.tsx
@@ -1,3 +1,5 @@
+import './terminalComponent.css';
+
 function TerminalComponent() {
     return(
         <div className="terminal">
@@ -5,17 +7,90 @@ function TerminalComponent() {
                 <p>main.ts</p>
             </div>
             <pre className="terminal__body">
+                <br />
                 <ol>
-                    <li className="line" data-status="empty">/////////</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">/</li>
-                    <li className="line" data-status="empty">//////////</li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">/**</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">*</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">* Eryk Olsza 2023</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">*</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">* https://github.com/Kyt00n</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">*</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">**/</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="newline"> </span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="comment">/////////////////////////////</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="keyword">enum</span>
+                        <span className="normal">Menu &#123;</span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        
+                    </li>
+                    <li className="line" data-status="empty">
+                        <a href="#">
+                            <span className="comment">//Portfolio Top Page</span><br />
+                            <span className="highlight">TOP</span><br />
+                            <span className="normal">,</span>
+                        </a>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <a href="#">
+                            <span className="comment">//A few facts about me</span><br />
+                            <span className="highlight">ABOUT_ME</span><br />
+                            <span className="normal">,</span>
+                        </a>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <a href="#">
+                            <span className="comment">//Preview of some of my projects</span><br />
+                            <span className="highlight">PROJECTS</span><br />
+                            <span className="normal">,</span>
+                        </a>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <a href="#">
+                            <span className="comment">//Preview of some of my skills</span><br />
+                            <span className="highlight">SKILLS</span><br />
+                            <span className="normal">,</span>
+                        </a>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <a href="#">
+                            <span className="comment">//my contact details</span><br />
+                            <span className="highlight">CONTACT_ME</span><br />
+                            <span className="normal">,</span>
+                        </a>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="normal">&#125;</span>
+                    </li>
+                    <li className="line" data-status="empty">/////////////////////////////</li>
+                    <li className="line" data-status="empty">
+                        <span className="keyword">const</span>
+                        <span className="normal"> gui = new dat.GUI() </span>
+                    </li>
+                    <li className="line" data-status="empty">
+                        <span className="keyword">const</span>
+                        <span className="normal"> canvas = document.querySelector('canvas.webgl') </span>
+                    </li>
+                    <br />
                 </ol>
             </pre>
             <div className="terminal__footer">
